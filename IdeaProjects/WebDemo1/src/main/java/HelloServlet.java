2b319a1544985bca349c91583421acead2195876
wushaonan <545328326@qq.com>
2016-06-14 15:26:50 +0800

    add

---
+++ IdeaProjects/WebDemo1/src/main/java/HelloServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Sn_Wu on 2016/6/13.
+ */
+@WebServlet(name="hello",urlPatterns={"/hello"})
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/jsp/hello.jsp").forward(req,resp);
+    }
+}
